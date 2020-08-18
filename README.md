2d4089f6d9a9cddef61fc45d53717a812cdb25aa
Ryan Meharg <ryan.meharg@altoros.com>
2020-08-18 15:36:22 +0100

    Cleanup for TKGI-only-install

--- README.md
+++ README.md
@@ -2,32 +2,17 @@ NSX-T terraform
 ---
 
 ## What does this repository provide?
-Terraform scripts that prepares the NSX-T environment for deploying OSS Concourse, OpsManager, TAS, TKG and TKGI
+Terraform scripts that prepares the NSX-T environment for deploying OSS Concourse, OpsManager and TKGI
 
 ## Pre-requisites:
-* Use terraform > 0.11.xx
+* Use terraform > 0.12.xx
 * Prepare the hosts
 * Deploy the edges and edge cluster
 * Fill in the details in `terraform.tfvars`
-* Create the Load Balancer Active Monitors, Networking > Load Balancing > Monitors > Add Active Monitors
-
-  | TYPE | NAME | MONITORING PORT |
-  | -- | -- | -- |
-  | `TCP` | `ROUTERS` | 8080 |
-  | `TCP` | `DIEGO_BRAINS` | 2222 |
-  | `TCP` | `ISTIO_ROUTERS` | 8080 |
 
 ## What happens upon terraforming?
 
 ```
-go get -u github.com/terraform-providers/terraform-provider-nsxt
-
-go install github.com/terraform-providers/terraform-provider-nsxt
-
-terraform init
-
-cp $GOBIN/terraform-provider-nsxt .terraform/plugins/darwin_amd64/terraform-provider-nsxt_v2.1.1_x4
-
 terraform init
 
 terraform plan -out=nsx
@@ -39,43 +24,25 @@ When the `apply` is executed, the following objects are created and tagged:
 
 - 1 T0 Gateway
 
-- 6 T1 Gateway
-  - AUTOMATION
-  - INFRASTRUCTURE
-  - DEPLOYMENT
-  - SERVICES
+- 3 T1 Gateway
+  - AUTOMATION (Concourse)
+  - INFRASTRUCTURE (OpsManager)
   - TKGI
-  - TKG
 
-- 7 Segments (Switches)
+- 4 Segments (Switches)
   - AUTOMATION
   - INFRASTRUCTURE
-  - DEPLOYMENT
-  - SERVICES
   - TKGI
-  - TKG
   - UPLINKS
 
-- External IP Pools for
-  - SNAT for TAS
+- External IP Pool for
   - SNAT for TKGI
 
 - NAT Rules
   - SNAT private CIDR's to external
   - DNAT to OpsManager VM
   - DNAT to TKGI API
-  - DNAT to Harbor API
   - DNAT to Jumpbox VM
   - DNAT to Concourse Web
 
 - 1 Small load balancer
-
-- 3 Virtual Servers
-  - GoRouters
-  - Diego Brains
-  - Istio Routers
-
-- 3 Server Pools, without members and using AutoMap
-  - GoRouters (monitor port 8080)
-  - Diego Brains (monitor port 2222)
-  - Istio Routers (monitor port 8080)
--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "nsxt" {
 }
 
 module "t0_gateway" {
-  source = "../modules/t0"
+  source = "./modules/t0"
 
   edge_cluster              = var.edge_cluster
   vlan_tz                   = var.vlan_tz
@@ -24,7 +24,7 @@ module "t0_gateway" {
 }
 
 module "t1_gateway_automation" {
-  source = "../modules/t1"
+  source = "./modules/t1"
 
   edge_cluster    = var.edge_cluster
   t0_gateway_path = module.t0_gateway.path
@@ -34,7 +34,7 @@ module "t1_gateway_automation" {
 }
 
 module "t1_gateway_infra" {
-  source = "../modules/t1"
+  source = "./modules/t1"
 
   edge_cluster    = var.edge_cluster
   t0_gateway_path = module.t0_gateway.path
@@ -43,28 +43,8 @@ module "t1_gateway_infra" {
   t1_gateway_cidr = var.t1_gateway_infra_cidr
 }
 
-module "t1_gateway_tas" {
-  source = "../modules/t1"
-
-  edge_cluster    = var.edge_cluster
-  t0_gateway_path = module.t0_gateway.path
-  overlay_tz      = var.overlay_tz
-  t1_gateway_name = var.t1_gateway_tas
-  t1_gateway_cidr = var.t1_gateway_tas_cidr
-}
-
-module "t1_gateway_services" {
-  source = "../modules/t1"
-
-  edge_cluster    = var.edge_cluster
-  t0_gateway_path = module.t0_gateway.path
-  overlay_tz      = var.overlay_tz
-  t1_gateway_name = var.t1_gateway_services
-  t1_gateway_cidr = var.t1_gateway_services_cidr
-}
-
 module "t1_gateway_tkgi" {
-  source = "../modules/t1"
+  source = "./modules/t1"
 
   edge_cluster    = var.edge_cluster
   t0_gateway_path = module.t0_gateway.path
@@ -73,18 +53,8 @@ module "t1_gateway_tkgi" {
   t1_gateway_cidr = var.t1_gateway_tkgi_cidr
 }
 
-module "t1_gateway_tkg" {
-  source = "../modules/t1"
-
-  edge_cluster    = var.edge_cluster
-  t0_gateway_path = module.t0_gateway.path
-  overlay_tz      = var.overlay_tz
-  t1_gateway_name = var.t1_gateway_tkg
-  t1_gateway_cidr = var.t1_gateway_tkg_cidr
-}
-
 module "snat_rule" {
-  source = "../modules/snat"
+  source = "./modules/snat"
 
   t0_gateway_path = module.t0_gateway.path
   rule_name       = var.snat_rule_name
@@ -93,7 +63,7 @@ module "snat_rule" {
 }
 
 module "jumpbox_dnat_rule" {
-  source = "../modules/dnat"
+  source = "./modules/dnat"
 
   t0_gateway_path = module.t0_gateway.path
   rule_name       = var.jumpbox_dnat_rule_name
@@ -102,7 +72,7 @@ module "jumpbox_dnat_rule" {
 }
 
 module "concourse_dnat_rule" {
-  source = "../modules/dnat"
+  source = "./modules/dnat"
 
   t0_gateway_path = module.t0_gateway.path
   rule_name       = var.concourse_dnat_rule_name
@@ -111,7 +81,7 @@ module "concourse_dnat_rule" {
 }
 
 module "opsmanager_dnat_rule" {
-  source = "../modules/dnat"
+  source = "./modules/dnat"
 
   t0_gateway_path = module.t0_gateway.path
   rule_name       = var.opsmanager_dnat_rule_name
@@ -120,7 +90,7 @@ module "opsmanager_dnat_rule" {
 }
 
 module "tkgi_dnat_rule" {
-  source = "../modules/dnat"
+  source = "./modules/dnat"
 
   t0_gateway_path = module.t0_gateway.path
   rule_name       = var.tkgi_dnat_rule_name
@@ -128,48 +98,22 @@ module "tkgi_dnat_rule" {
   private_ip      = var.tkgi_api_private_ip
 }
 
-module "harbor_dnat_rule" {
-  source = "../modules/dnat"
-
-  t0_gateway_path = module.t0_gateway.path
-  rule_name       = var.harbor_dnat_rule_name
-  public_ip       = var.harbor_public_ip
-  private_ip      = var.harbor_private_ip
-}
-
-module "tas_ip_block" {
-  source = "../modules/ip-block"
-
-  ip_block_name = var.tas_ip_block_name
-  ip_block_cidr = var.tas_ip_block_cidr
-}
-
 module "tkgi_nodes_ip_block" {
-  source = "../modules/ip-block"
+  source = "./modules/ip-block"
 
   ip_block_name = var.tkgi_nodes_ip_block_name
   ip_block_cidr = var.tkgi_nodes_ip_block_cidr
 }
 
 module "tkgi_pods_ip_block" {
-  source = "../modules/ip-block"
+  source = "./modules/ip-block"
 
   ip_block_name = var.tkgi_pods_ip_block_name
   ip_block_cidr = var.tkgi_pods_ip_block_cidr
 }
 
-module "tas_snat_ip_pool" {
-  source = "../modules/ip-pool"
-
-  ip_pool_name        = var.tas_snat_ip_pool_name
-  ip_pool_cidr        = var.tas_snat_ip_pool_cidr
-  ip_pool_gateway     = var.tas_snat_ip_pool_gateway
-  ip_pool_range_start = var.tas_snat_ip_pool_range_start
-  ip_pool_range_end   = var.tas_snat_ip_pool_range_end
-}
-
 module "tkgi_snat_ip_pool" {
-  source = "../modules/ip-pool"
+  source = "./modules/ip-pool"
 
   ip_pool_name        = var.tkgi_snat_ip_pool_name
   ip_pool_cidr        = var.tkgi_snat_ip_pool_cidr
@@ -179,48 +123,9 @@ module "tkgi_snat_ip_pool" {
 }
 
 module "workload_lb" {
-  source = "../modules/loadbalancer"
+  source = "./modules/loadbalancer"
 
   lb_name         = var.workload_lb_name
   lb_type         = var.workload_lb_type
   t1_gateway_path = module.t1_gateway_infra.path
-}
-
-module "routers_nsgroups_pool_virtualserver" {
-  source = "../modules/nsgroups-pool-virtualserver"
-
-  ns_group_name           = var.router_ns_group_name
-  server_pool_name        = var.router_server_pool_name
-  public_ip               = var.router_public_ip
-  ports                   = ["443"]
-  lb_path                 = module.workload_lb.path
-  app_profile             = var.router_lb_application_profile
-  active_lb_monitor_name  = var.router_active_lb_monitor_name
-  passive_lb_monitor_name = var.router_passive_lb_monitor_name
-}
-
-module "diego_brains_nsgroups_pool_virtualserver" {
-  source = "../modules/nsgroups-pool-virtualserver"
-
-  ns_group_name           = var.diego_brain_ns_group_name
-  server_pool_name        = var.diego_brain_server_pool_name
-  public_ip               = var.diego_brain_public_ip
-  ports                   = ["2222"]
-  lb_path                 = module.workload_lb.path
-  app_profile             = var.diego_brain_lb_application_profile
-  active_lb_monitor_name  = var.diego_brain_active_lb_monitor_name
-  passive_lb_monitor_name = var.diego_brain_passive_lb_monitor_name
-}
-
-module "istio_routers_nsgroups_pool_virtualserver" {
-  source = "../modules/nsgroups-pool-virtualserver"
-
-  ns_group_name           = var.istio_router_ns_group_name
-  server_pool_name        = var.istio_router_server_pool_name
-  public_ip               = var.istio_router_server_public_ip
-  ports                   = ["443"]
-  lb_path                 = module.workload_lb.path
-  app_profile             = var.istio_router_lb_application_profile
-  active_lb_monitor_name  = var.istio_router_active_lb_monitor_name
-  passive_lb_monitor_name = var.istio_router_passive_lb_monitor_name
 }
